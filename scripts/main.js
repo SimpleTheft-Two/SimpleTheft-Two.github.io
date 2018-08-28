abb6dd73d273f0abceb672c369a544d6acf007f4
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-08-28 16:23:28 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,7 @@ function liquorTown(){
   var lttake = "$13,882";
   document.getElementById("ltTake").innerHTML = "Take = " + lttake;
 }
+function wilsonLiquor(){
+  var wltake = "$9,862";
+  document.getElementById("wlTake").innerHTML = "Take = " + wltake;
+}
