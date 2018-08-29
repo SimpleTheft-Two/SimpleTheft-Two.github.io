f3db3c4bf3ea90bc38c30cdd02b598e27b606f1e
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-08-29 12:29:53 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+function clear(){
+  document.getElementById("clTake").innerHTML = "";
+  document.getElementById("ltTake").innerHTML = "";
+  document.getElementById("wlTake").innerHTML = "";
+  document.getElementById("lmTake").innerHTML = "";
+  document.getElementById("mlTake").innerHTML = "";
+  document.getElementById("deTake").innerHTML = "";
+  document.getElementById("wTake").innerHTML = "";
+}
+/*
+Large-Size Liquor
+*/
 function crystaLiquor(){
   var cltake = "$14,942";
   document.getElementById("clTake").innerHTML = "Take = " + cltake;
@@ -22,6 +34,9 @@ function wilsonLiquor(){
   var wltake = "$9,862";
   document.getElementById("wlTake").innerHTML = "Take = " + wltake;
 }
+/*
+Medium-Size Liquor
+*/
 function liquorMart(){
   var lmtake = "$8,048";
   document.getElementById("lmTake").innerHTML = "Take = " + lmtake;
@@ -34,6 +49,9 @@ function drunkenEagle(){
   var detake = "$8,478";
   document.getElementById("deTake").innerHTML = "Take = " + detake;
 }
+/*
+Small-Size Liquor
+*/
 function woodfortLiquor(){
   var wtake = "$2,277";
   document.getElementById("wTake").innerHTML = "Take = " + wtake;
