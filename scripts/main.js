3f100ce073de9d012323127fe798af1e49eafbdf
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-08-29 12:34:13 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ function clear(){
   document.getElementById("mlTake").innerHTML = "";
   document.getElementById("deTake").innerHTML = "";
   document.getElementById("wTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
 }
 /*
 Large-Size Liquor
@@ -19,6 +20,7 @@ function crystaLiquor(){
   document.getElementById("mlTake").innerHTML = "";
   document.getElementById("deTake").innerHTML = "";
   document.getElementById("wTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
 }
 function liquorTown(){
   var lttake = "$13,882";
@@ -29,10 +31,18 @@ function liquorTown(){
   document.getElementById("mlTake").innerHTML = "";
   document.getElementById("deTake").innerHTML = "";
   document.getElementById("wTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
 }
 function wilsonLiquor(){
   var wltake = "$9,862";
   document.getElementById("wlTake").innerHTML = "Take = " + wltake;
+  document.getElementById("clTake").innerHTML = "";
+  document.getElementById("ltTake").innerHTML = "";
+  document.getElementById("lmTake").innerHTML = "";
+  document.getElementById("mlTake").innerHTML = "";
+  document.getElementById("deTake").innerHTML = "";
+  document.getElementById("wTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
 }
 /*
 Medium-Size Liquor
@@ -40,14 +50,35 @@ Medium-Size Liquor
 function liquorMart(){
   var lmtake = "$8,048";
   document.getElementById("lmTake").innerHTML = "Take = " + lmtake;
+  document.getElementById("clTake").innerHTML = "";
+  document.getElementById("ltTake").innerHTML = "";
+  document.getElementById("wlTake").innerHTML = "";
+  document.getElementById("mlTake").innerHTML = "";
+  document.getElementById("deTake").innerHTML = "";
+  document.getElementById("wTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
 }
 function marketLiquor(){
   var mltake = "$5,450";
   document.getElementById("mlTake").innerHTML = "Take = " + mltake;
+  document.getElementById("clTake").innerHTML = "";
+  document.getElementById("ltTake").innerHTML = "";
+  document.getElementById("wlTake").innerHTML = "";
+  document.getElementById("lmTake").innerHTML = "";
+  document.getElementById("deTake").innerHTML = "";
+  document.getElementById("wTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
 }
 function drunkenEagle(){
   var detake = "$8,478";
   document.getElementById("deTake").innerHTML = "Take = " + detake;
+  document.getElementById("clTake").innerHTML = "";
+  document.getElementById("ltTake").innerHTML = "";
+  document.getElementById("wlTake").innerHTML = "";
+  document.getElementById("lmTake").innerHTML = "";
+  document.getElementById("mlTake").innerHTML = "";
+  document.getElementById("wTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
 }
 /*
 Small-Size Liquor
@@ -55,4 +86,22 @@ Small-Size Liquor
 function woodfortLiquor(){
   var wtake = "$2,277";
   document.getElementById("wTake").innerHTML = "Take = " + wtake;
+  document.getElementById("clTake").innerHTML = "";
+  document.getElementById("ltTake").innerHTML = "";
+  document.getElementById("wlTake").innerHTML = "";
+  document.getElementById("lmTake").innerHTML = "";
+  document.getElementById("mlTake").innerHTML = "";
+  document.getElementById("deTake").innerHTML = "";
+  document.getElementById("flTake").innerHTML = "";
+}
+function fallsworthLiquor(){
+  var fltake = "$";
+  document.getElementById("flTake").innerHTML = "Take = " + fltake;
+  document.getElementById("clTake").innerHTML = "";
+  document.getElementById("ltTake").innerHTML = "";
+  document.getElementById("wlTake").innerHTML = "";
+  document.getElementById("lmTake").innerHTML = "";
+  document.getElementById("mlTake").innerHTML = "";
+  document.getElementById("deTake").innerHTML = "";
+  document.getElementById("wTake").innerHTML = "";
 }
