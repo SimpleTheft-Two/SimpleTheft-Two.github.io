35be2aca5b4044ebb50ac524d042db2332b10b68
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-09-03 22:36:30 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,41 @@
 $(function(){
   //Large Size Liquor
   $.fn.crystaliquor = function (){
-    var cltake = "$";
+    var cltake = "$14,942";
     $("#clTake").html("Take = " + cltake);
   }
   $.fn.liquortown = function (){
-    var lttake = "$";
+    var lttake = "$13,882";
     $("#ltTake").html("Take = " + lttake);
   }
   $.fn.wilsonliquor = function (){
-    var wltake = "$";
+    var wltake = "$9,862";
     $("#wlTake").html("Take = " + wltake);
   }
   //Medium Size Liquor
   $.fn.liquormart = function (){
-    var lmtake = "$";
+    var lmtake = "$8,048";
     $("#lmTake").html("Take = " + lmtake);
   }
   $.fn.marketliquor = function (){
-    var mltake = "$";
+    var mltake = "$5,450";
     $("#mlTake").html("Take = " + mltake);
   }
   $.fn.drunkeneagle = function (){
-    var detake = "$";
+    var detake = "$8,478";
     $("#deTake").html("Take = " + detake);
   }
   //Small Size Liquor
   $.fn.woodfortliquor = function (){
-    var wtake = "$";
+    var wtake = "$2,277";
     $("#wTake").html("Take = " + wtake);
   }
   $.fn.fallsworthliquor = function (){
-    var fltake = "$";
+    var fltake = "$2,050";
     $("#flTake").html("Take = " + fltake);
   }
   $.fn.drunkWest = function (){
-    var dwtake = "$";
+    var dwtake = "$1,752";
     $("#dwTake").html("Take = " + dwtake);
   }
   //Large Size Jewelry
@@ -50,44 +50,5 @@ $(function(){
   $.fn.diamondsea = function (){
     var dstake = "$";
     $("#dsTake").html("Take = " + dstake);
-  }
-  //Small Size Banks
-  $.fn.shoresidebank = function (){
-    var sbtake = "$";
-    $("#sbTake").html("Take = " + sbtake);
-  }
-  $.fn.woodfortBank = function (){
-    var wbtake = "$";
-    $("#wbTake").html("Take = " + wbtake);
-  }
-  $.fn.bestregister = function (){
-    var brtake = "$";
-    $("#brTake").html("Take = " + brtake);
-  }
-  //Medium Size Banks
-  $.fn.julietschest = function (){
-    var jctake = "$";
-    $("#jcTake").html("Take = " + jctake);
-  }
-  $.fn.excelbank = function (){
-    var ebtake = "$";
-    $("#ebTake").html("Take = " + ebtake);
-  }
-  $.fn.treasurechest = function (){
-    var tctake = "$";
-    $("#tcTake").html("Take = " + tctake);
-  }
-  //Large Size Banks
-  $.fn.westbank = function (){
-    var wbtaket = "$";
-    $("#wbTakeT").html("Take = " + wbtaket);
-  }
-  $.fn.jefferiesbank = function (){
-    var jbtake = "$";
-    $("#jbTake").html("Take = " + jbtake);
-  }
-  $.fn.roseholdings = function (){
-    var rhtake = "$";
-    $("#rhTake").html("Take = " + rhtake);
   }
 });
