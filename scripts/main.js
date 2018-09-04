281dca63b1d67b9c0f1746528d1f29a62025c572
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-09-04 10:39:32 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,118 +1,118 @@
 $(function(){
   //Large Size Liquor
-  $("clButton").click(function(){
+  $("#clButton").click(function(){
     var cltake = "$14,942";
     $("#clTake").html("Take = " + cltake);
   });
-  $("ltButton").click(function(){
+  $("#ltButton").click(function(){
     var lttake = "$13,882";
     $("#ltTake").html("Take = " + lttake);
   });
-  $("wlButton").click(function(){
+  $("#wlButton").click(function(){
     var wltake = "$9,862";
     $("#wlTake").html("Take = " + wltake);
   });
   //Medium Size Liquor
-  $("lmButton").click(function(){
+  $("#lmButton").click(function(){
     var lmtake = "$8,048";
     $("#lmTake").html("Take = " + lmtake);
   });
-  $("mlButton").click(function(){
+  $("#mlButton").click(function(){
     var mltake = "$5,450";
     $("#mlTake").html("Take = " + mltake);
   });
-  $("deButton").click(function(){
+  $("#deButton").click(function(){
     var detake = "$8,478";
     $("#deTake").html("Take = " + detake);
   });
   //Small Size Liquor
-  $("wButton").click(function(){
+  $("#wButton").click(function(){
     var wtake = "$2,277";
     $("#wTake").html("Take = " + wtake);
   });
-  $("flButton").click(function(){
+  $("#flButton").click(function(){
     var fltake = "$2,050";
     $("#flTake").html("Take = " + fltake);
   });
-  $("dwButton").click(function(){
+  $("#dwButton").click(function(){
     var dwtake = "$1,752";
     $("#dwTake").html("Take = " + dwtake);
   });
   //Large Size Jewelry
-  $("ggButton").click(function(){
+  $("#ggButton").click(function(){
     var ggtake = "$";
     $("#ggTake").html("Take = " + ggtake);
   });
-  $("glButton").click(function(){
+  $("#glButton").click(function(){
     var gltake = "$";
     $("#glTake").html("Take = " + gltake);
   });
-  $("dsButton").click(function(){
+  $("#dsButton").click(function(){
     var dstake = "$";
     $("#dsTake").html("Take = " + dstake);
   });
   //Medium Size Jewelry
-  $("hrButton").click(function(){
+  $("#hrButton").click(function(){
     var hrtake = "$1,968,880";
     $("#hrTake").html("Take = " + hrtake);
   });
-  $("srButton").click(function(){
+  $("#srButton").click(function(){
     var srtake = "$1,012,324";
     $("#srTake").html("Take = " + srtake);
   });
-  $("ejButton").click(function(){
+  $("#ejButton").click(function(){
     var ejtake = "$980,864";
     $("#ejTake").html("Take = " + ejtake);
   });
   //Small Size Jewelry
-  $("ehButton").click(function(){
+  $("#ehButton").click(function(){
     var ehtake = "$722,282";
     $("#ehTake").html("Take = " + ehtake);
   });
-  $("hsButton").click(function(){
+  $("#hsButton").click(function(){
     var hstake = "$682,844";
     $("#hsTake").html("Take = " + hstake);
   });
-  $("jlButton").click(function(){
+  $("#jlButton").click(function(){
     var jltake = "$657,910";
     $("#jlTake").html("Take = " + jltake);
   });
   //Small Size Banks
-  $("sbButton").click(function(){
+  $("#sbButton").click(function(){
     var sbtake = "$9,615,499";
     $("#sbTake").html("Take = " + sbtake);
   });
-  $("wbButton").click(function(){
+  $("#wbButton").click(function(){
     var wbtake = "$10,418,984";
     $("#wbTake").html("Take = " + wbtake);
   });
-  $("brButton").click(function(){
+  $("#brButton").click(function(){
     var brtake = "$13,694,689";
     $("#brTake").html("Take = " + brtake);
   });
   //Medium Size Banks
-  $("jtButton").click(function(){
+  $("#jtButton").click(function(){
     var jttake = "$15,324,859";
     $("#jtTake").html("Take = " + jttake);
   });
-  $("ebButton").click(function(){
+  $("#ebButton").click(function(){
     var ebtake = "$19,159,492";
     $("#ebTake").html("Take = " + ebtake);
   });
-  $("tcButton").click(function(){
+  $("#tcButton").click(function(){
     var tctake = "$23,535,524";
     $("#tcTake").html("Take = " + tctake);
   });
   //Large Size Banks
-  $("wbtButton").click(function(){
+  $("#wbtButton").click(function(){
     var wbtaket = "$25,036,258";
     $("#wbTakeT").html("Take = " + wbtaket);
   });
-  $("jbButton").click(function(){
+  $("#jbButton").click(function(){
     var jbtake = "$29,275,561";
     $("#jbTake").html("Take = " + jbtake);
   });
-  $("rhButton").click(function(){
+  $("#rhButton").click(function(){
     var rhtake = "$34,205,551";
     $("#rhTake").html("Take = " + rhtake);
   });
