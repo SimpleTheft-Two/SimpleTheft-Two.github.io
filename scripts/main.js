4bc4df60ca6878f4183439d1387a27f51fed9407
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-09-04 10:37:07 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,149 +1,119 @@
 $(function(){
   //Large Size Liquor
-  $.fn.crystaliquor = function (){
+  $("clButton").click(function(){
     var cltake = "$14,942";
     $("#clTake").html("Take = " + cltake);
-  }
-  $.fn.liquortown = function (){
+  });
+  $("ltButton").click(function(){
     var lttake = "$13,882";
     $("#ltTake").html("Take = " + lttake);
-  }
-  $.fn.wilsonliquor = function (){
+  });
+  $("wlButton").click(function(){
     var wltake = "$9,862";
     $("#wlTake").html("Take = " + wltake);
-  }
+  });
   //Medium Size Liquor
-  $.fn.liquormart = function (){
+  $("lmButton").click(function(){
     var lmtake = "$8,048";
     $("#lmTake").html("Take = " + lmtake);
-  }
-  $.fn.marketliquor = function (){
+  });
+  $("mlButton").click(function(){
     var mltake = "$5,450";
     $("#mlTake").html("Take = " + mltake);
-  }
-  $.fn.drunkeneagle = function (){
+  });
+  $("deButton").click(function(){
     var detake = "$8,478";
     $("#deTake").html("Take = " + detake);
-  }
+  });
   //Small Size Liquor
-  $.fn.woodfortliquor = function (){
+  $("wButton").click(function(){
     var wtake = "$2,277";
     $("#wTake").html("Take = " + wtake);
-  }
-  $.fn.fallsworthliquor = function (){
+  });
+  $("flButton").click(function(){
     var fltake = "$2,050";
     $("#flTake").html("Take = " + fltake);
-  }
-  $.fn.drunkWest = function (){
+  });
+  $("dwButton").click(function(){
     var dwtake = "$1,752";
     $("#dwTake").html("Take = " + dwtake);
-  }
+  });
   //Large Size Jewelry
-  $.fn.goldengoose = function (){
+  $("ggButton").click(function(){
     var ggtake = "$";
     $("#ggTake").html("Take = " + ggtake);
-  }
-  $.fn.goldlodge = function (){
+  });
+  $("glButton").click(function(){
     var gltake = "$";
     $("#glTake").html("Take = " + gltake);
-  }
-  $.fn.diamondsea = function (){
+  });
+  $("dsButton").click(function(){
     var dstake = "$";
     $("#dsTake").html("Take = " + dstake);
-  }
+  });
   //Medium Size Jewelry
-  $.fn.hardrocks = function (){
+  $("hrButton").click(function(){
     var hrtake = "$1,968,880";
     $("#hrTake").html("Take = " + hrtake);
-  }
-  $.fn.searocks = function (){
+  });
+  $("srButton").click(function(){
     var srtake = "$1,012,324";
     $("#srTake").html("Take = " + srtake);
-  }
-  $.fn.engagementjewelry = function (){
+  });
+  $("ejButton").click(function(){
     var ejtake = "$980,864";
     $("#ejTake").html("Take = " + ejtake);
-  }
+  });
   //Small Size Jewelry
-  $.fn.engravedheart = function (){
+  $("ehButton").click(function(){
     var ehtake = "$722,282";
     $("#ehTake").html("Take = " + ehtake);
-  }
-  $.fn.heartstone = function (){
+  });
+  $("hsButton").click(function(){
     var hstake = "$682,844";
     $("#hsTake").html("Take = " + hstake);
-  }
-  $.fn.jewelove = function (){
+  });
+  $("jlButton").click(function(){
     var jltake = "$657,910";
     $("#jlTake").html("Take = " + jltake);
-  }
+  });
   //Small Size Banks
-  $.fn.shoresidebank = function (){
+  $("sbButton").click(function(){
     var sbtake = "$9,615,499";
     $("#sbTake").html("Take = " + sbtake);
-  }
-  $.fn.woodfortbank = function (){
+  });
+  $("wbButton").click(function(){
     var wbtake = "$10,418,984";
     $("#wbTake").html("Take = " + wbtake);
-  }
-  $.fn.bestregister = function (){
+  });
+  $("brButton").click(function(){
     var brtake = "$13,694,689";
     $("#brTake").html("Take = " + brtake);
-  }
+  });
   //Medium Size Banks
-  $.fn.julietstreasure = function (){
+  $("jtButton").click(function(){
     var jttake = "$15,324,859";
     $("#jtTake").html("Take = " + jttake);
-  }
-  $.fn.excelbank = function (){
+  });
+  $("ebButton").click(function(){
     var ebtake = "$19,159,492";
     $("#ebTake").html("Take = " + ebtake);
-  }
-  $.fn.treasurechest = function (){
+  });
+  $("tcButton").click(function(){
     var tctake = "$23,535,524";
     $("#tcTake").html("Take = " + tctake);
-  }
+  });
   //Large Size Banks
-  $.fn.westbank = function (){
+  $("wbtButton").click(function(){
     var wbtaket = "$25,036,258";
     $("#wbTakeT").html("Take = " + wbtaket);
-  }
-  $.fn.jefferiesbank = function (){
+  });
+  $("jbButton").click(function(){
     var jbtake = "$29,275,561";
     $("#jbTake").html("Take = " + jbtake);
-  }
-  $.fn.roseholdings = function (){
+  });
+  $("rhButton").click(function(){
     var rhtake = "$34,205,551";
     $("#rhTake").html("Take = " + rhtake);
-  }
-  //Liquor Buttons
-  $("#clButton").crystaliquor();
-  $("#ltButton").liquortown();
-  $("#wlButton").wilsonliquor();
-  $("#lmButton").liquormart();
-  $("#mlButton").marketliquor();
-  $("#deButton").drunkeneagle();
-  $("#wButton").woodfortliquor();
-  $("#flButton").fallsworthliquor();
-  $("#dwButton").drunkwest();
-  //Jewelry Buttons
-  $("#ggButton").goldengoose();
-  $("#glButton").goldlodge();
-  $("#dsButton").diamondsea();
-  $("#hrButton").hardrocks();
-  $("#srButton").searocks();
-  $("#ejButton").engagementjewelry();
-  $("#ehButton").engravedheart();
-  $("#hsButton").heartstone();
-  $("#jlButton").jewelove();
-  //Bank Buttons
-  $("#sbButton").shoresidebank();
-  $("#wbButton").woodfortbank();
-  $("#brButton").bestregister();
-  $("#jtButton").julietstreasure();
-  $("#ebButton").excelbank();
-  $("#tcButton").treasurechest();
-  $("#wbtButton").westbank();
-  $("#jbButton").jefferiesbank();
-  $("#rhButton").roseholdings();
+  });
 });
